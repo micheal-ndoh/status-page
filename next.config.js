53391cd2be6ae004b740c0f11054de9be2783c3d
micheal-ndoh <michaelndoh9@gmail.com>
2025-08-04 08:53:58 +0100

    Enhance Next.js and Vercel configurations for improved functionality
    - Added experimental support for external packages in Next.js configuration, specifically for '@prisma/client'.
    - Removed outdated function execution limits from Vercel configuration, streamlining deployment settings.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
     },
+    experimental: {
+        serverComponentsExternalPackages: ['@prisma/client'],
+    },
     images: {
         domains: [
             'localhost',
